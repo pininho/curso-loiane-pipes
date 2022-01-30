83807ebc8ac93600bfb2a219fa8d62ba369ca7d8
Alessandro Pino <alessandro@profissionalti.com>
2022-01-30 00:31:44 -0300

    usando pipes do Angular

---
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.scss']
+})
+export class ExemplosPipesComponent implements OnInit {
+
+  livro: any = {
+    titulo: 'Learning Javascript Data Structures and Algorithms 2nd ed',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23), //O indice do mês começa em 0. Então, junho é 5
+    url: 'http://a.co/glqjpRP'
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
